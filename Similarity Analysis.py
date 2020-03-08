facd6485da030b086250e2b2dcdc3c8f089a48e0
Danny O'Brien <dannyobrien@148-100-183-252.FoxNet.marist.edu>
2020-03-08 15:56:28 -0400

    Cleans up bugs.
    w2v model almost definitely needs tuning

--- Similarity Analysis.py
+++ Similarity Analysis.py
@@ -37,15 +37,10 @@ import numpy as np
 import time
 from os import path
 import argparse
+from gensim.models import Word2Vec
 
 
 
-
-# Load Google's pre-trained Word2Vec model.
-data_folder = Path("data/")
-file = data_folder / "GoogleNews-vectors-negative300.bin"
-model = gensim.models.KeyedVectors.load_word2vec_format(file, binary=True, limit=500000)
-index2word_set = set(model.wv.index2word)
 
 # This can get initialized up here, as it will be constant throughout. 
 count_vectorizer = CountVectorizer(stop_words='english')
@@ -111,7 +106,7 @@ class Corpus:
         self.fill_type = mode
                    
         
-    def similarity_analysis(self, is_smart, d2v_model):
+    def similarity_analysis(self, is_smart, d2v_model, w2v_model, index2word_set):
         """Function: Analyzes all corpus against corpus main article and returns results in pandas dataframe.
            ============================================================================
            Parameters
@@ -139,7 +134,7 @@ class Corpus:
         ## Actually do the analysis
         ndx = 1 ## index for d2v tagging purposes
         for article in self.articles:
-            results.loc[article.main_title] = self.main_article.similarity_analysis(article, is_smart, ndx, d2v_model)
+            results.loc[article.main_title] = self.main_article.similarity_analysis(article, is_smart, ndx, d2v_model, w2v_model, index2word_set)
             ndx = ndx + 1
         
         return results
@@ -218,7 +213,7 @@ class WikiArticle:
         self.word_frequency = {}
         
     
-    def similarity_analysis(self, article, is_smart, ndx, d2v_model):
+    def similarity_analysis(self, article, is_smart, ndx, d2v_model, w2v_model, index2word_set):
         """
         Function: To be used in Corpus class to perform analysis between main article and comparison article
         ============================================================================
@@ -231,8 +226,8 @@ class WikiArticle:
            Returns Pandas series to be used in forming a dataframe result."""
         ## Check main title tier
         if(is_smart == True):
-            a_vec = self.w2v_vector(self.main_title);
-            b_vec = article.w2v_vector(article.main_title);
+            a_vec = self.w2v_vector(self.main_title, w2v_model, index2word_set);
+            b_vec = article.w2v_vector(article.main_title, w2v_model, index2word_set);
             main_title_comparison = vec_cosine_analysis(a_vec, b_vec)
             if is_over_threshold(main_title_comparison):
                 main_title_tier = True
@@ -241,8 +236,8 @@ class WikiArticle:
 
             ## Check sub title tier
             article.get_secondary_titles()
-            a_vec = self.w2v_vector(self.secondary_titles)
-            b_vec = article.w2v_vector(article.secondary_titles)
+            a_vec = self.w2v_vector(self.secondary_titles, w2v_model, index2word_set)
+            b_vec = article.w2v_vector(article.secondary_titles, w2v_model, index2word_set)
             secondary_title_comparison = vec_cosine_analysis(a_vec, b_vec)
             if is_over_threshold(secondary_title_comparison):
                 secondary_title_tier = True
@@ -270,7 +265,7 @@ class WikiArticle:
             for secondary_title in self.soup.find_all("h2"):
                 self.secondary_titles += " " + secondary_title.get_text()
     
-    def w2v_vector(self, text):
+    def w2v_vector(self, text, model, index2word_set):
         """
         Function: To get word2vec average vector of provided text
         ============================================================================
@@ -281,7 +276,7 @@ class WikiArticle:
            Returns
            ----------
            Word2Vec average vector of provided text."""
-        num_features = 300;
+        num_features = 100;
         featureVec = np.zeros((num_features,), dtype="float32")
         nwords = 0
 
@@ -469,7 +464,7 @@ def build_w2v_model(corpus):
             for word in article.main_text:
                 training_corpus.append(word)
                 
-        w2v_model = Word2Vec(sentences=training_corpus, min_count = 10)
+        w2v_model = Word2Vec(sentences=training_corpus, min_count = 10, size=300)
         w2v_model.save("W2Vmodel")
         print("Word2Vec model was made with all_related corpus")
         return w2v_model
@@ -573,21 +568,23 @@ if __name__ == "__main__":
     corpus_fifty_fifty.fill_corpus(args.size, "fifty_fifty")
     
     ## Issue #1 in https://github.com/danielobrien3/HBC-Text-Similarity-Analysis
-    corpus.filter_corpus_by_frequency()
+    #corpus.filter_corpus_by_frequency()
     
     ## Loads model from file. The model is created if it does not yet exist. 
     d2v_model = build_d2v_model(corpus_related)
     w2v_model = build_w2v_model(corpus_related)
+    index2word_set = set(w2v_model.wv.index2word)
+    
     
     ## Get (multitiered AND regular analysis) results of each corpus
-    smart_results_related = corpus_related.similarity_analysis(True)
-    dumb_results_related = corpus_related.similarity_analysis(False)
+    smart_results_related = corpus_related.similarity_analysis(True, d2v_model, w2v_model, index2word_set)
+    dumb_results_related = corpus_related.similarity_analysis(False, d2v_model, w2v_model, index2word_set)
     
-    smart_results_random = corpus_random.similarity_analysis(True)
-    dumb_results_random = corpus_random.similarity_analysis(False)
+    smart_results_random = corpus_random.similarity_analysis(True, d2v_model, w2v_model, index2word_set)
+    dumb_results_random = corpus_random.similarity_analysis(False, d2v_model, w2v_model, index2word_set)
     
-    smart_results_fifty = corpus_fifty_fifty.similarity_analysis(True)
-    dumb_results_fifty = corpus_fifty_fifty.similarity_analysis(False)
+    smart_results_fifty = corpus_fifty_fifty.similarity_analysis(True, d2v_model, w2v_model, index2word_set)
+    dumb_results_fifty = corpus_fifty_fifty.similarity_analysis(False, d2v_model, w2v_model, index2word_set)
     
     
     end = time.time()
@@ -599,7 +596,7 @@ if __name__ == "__main__":
     dumb_related_file = fileTwo = "regular_"  + str(args.size) + "_related.csv"
     
     smart_random_file = "multitiered_"  + str(args.size) + "_random.csv"
-    dumb_related_file = "regular_"  + str(args.size) + "_random.csv"
+    dumb_random_file = "regular_"  + str(args.size) + "_random.csv"
     
     smart_fifty_fifty_file = "multitiered_"  + str(args.size) + "_fifty_fifty.csv"
     dumb_fifty_fifty_file = "regular_"  + str(args.size) + "_fifty_fifty.csv"
